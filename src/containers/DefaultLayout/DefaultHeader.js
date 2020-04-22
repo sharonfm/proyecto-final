b21bbff18264dee3d856e98e9dc863e4fc1d0020
Ycristian01 <ycristian@uninorte.edu.co>
2020-04-21 22:23:52 -0500

    actualizado login. Falta corregir un error con el logout

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav } from 'reactstrap';
 import PropTypes from 'prop-types';
-
+import { auth } from "../../services/firebase";
 import {  AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
 import logo from '../../assets/img/brand/logo.svg'
 
@@ -13,36 +13,35 @@ const defaultProps = {};
 /**
  * Esto contiene el Navbar y lo que se depliega en la foto
  */
-class DefaultHeader extends Component {
-  render() {
+const DefaultHeader =(props) => {
 
-    // eslint-disable-next-line
-    const { children, ...attributes } = this.props;
+  // eslint-disable-next-line
+  const { children, ...attributes } = props;
 
-    return (
-      <React.Fragment>
-        <AppSidebarToggler className="d-lg-none" display="md" mobile />
-        <AppNavbarBrand
-          full={{ src: logo, width: 70, height: 50, alt: 'Project Logo' }}
-          minimized={{ src: logo, width: 50, height: 50, alt: 'Project Logo' }}
-        />
-        <AppSidebarToggler className="d-md-down-none" display="lg" />
-        
-        <Nav className="ml-auto" navbar>
-          <UncontrolledDropdown nav direction="down">
-            <DropdownToggle nav>
-              <img src={'../../assets/img/avatars/usuario.png'} Alt='Usuario' className="img-avatar"/>
-            </DropdownToggle>
-            <DropdownMenu right>             
-              <DropdownItem header tag="div" className="text-center" style= {{backgroundColor:'#43425D'}}><strong style={{color:'#F2F2F2'}}>Ajustes</strong></DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i> <medium style={{color:'#43425D'}}> Cerrar Sesión</medium></DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <AppSidebarToggler className="d-lg-none" display="md" mobile />
+      <AppNavbarBrand
+        full={{ src: logo, width: 70, height: 50, alt: 'Project Logo' }}
+        minimized={{ src: logo, width: 50, height: 50, alt: 'Project Logo' }}
+      />
+      <AppSidebarToggler className="d-md-down-none" display="lg" />
+
+      <Nav className="ml-auto" navbar>
+        <UncontrolledDropdown nav direction="down">
+          <DropdownToggle nav>
+            <img src={'../../assets/img/avatars/usuario.png'} Alt='Usuario' className="img-avatar" />
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem header tag="div" className="text-center" style={{ backgroundColor: '#43425D' }}><strong style={{ color: '#F2F2F2' }}>Ajustes</strong></DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem onClick={()=>{auth.signOut()}} > <i className="fa fa-lock"></i> <medium style={{ color: '#43425D' }}> Cerrar Sesión</medium></DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
+    </React.Fragment>
+  );
+
 }
 
 DefaultHeader.propTypes = propTypes;
